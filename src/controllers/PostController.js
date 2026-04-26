025b2f2f2b52c38beefbadf26390372939524dab
Charumehra <174786887+Charumehra@users.noreply.github.com>
2026-04-26 18:19:01 +0530

    Setup Backend

---
+++ src/controllers/PostController.js
@@ -0,0 +1,36 @@
+const Post = require('../models/post');
+
+const createPost = async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+    res.status(201).json({ message: 'Post created successfully', data: newPost });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const getPosts = async(req, res)=>{
+    try{
+        const posts = await Post.find()
+        res.status(200).json({ message:'Posts fetched successfully', data:posts })
+    }catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+
+const deletePost = async (req, res)=>{
+    try{
+        const PostId = req.params.id
+        await Post.findByIdAndDelete(PostId)
+        res.status(200).json({message:'Post deleted successfully'})
+  }catch (error) {
+  res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports = {
+  createPost,
+    getPosts,
+    deletePost
+};
